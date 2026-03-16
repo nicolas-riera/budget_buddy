eb21d8e770c9136be776dc42e683a2ab6d7d5438
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-16 10:38:24 +0100

    wip: added initial ctk structure

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.BBApp import *
+
+if __name__ == "__main__":
+    app = BBApp()
+    app.ctk.mainloop()
---
+++ src/BBApp.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+
+class BBApp:
+    def __init__(self):
+        self.ctk = ctk.CTk()
+
+        self.ctk.title("Budget Buddy")
+        self.ctk.geometry("1280x720")
