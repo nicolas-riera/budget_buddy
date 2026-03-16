cd5bd229536ae9e219f706637981dfc80cfced62
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-03-16 15:44:57 +0100

    feat: implement DatabaseManager for MySQL connections
       - Add DatabaseManager class to handle database connection and queries.
       - Load credentials from environment variables using dotenv.
       - Implement request and close_db methods for basic database operations.

---
+++ src/DatabaseManager.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import os 
+import dotenv
+
+class DatabaseManager: 
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.db_connect = mysql.connector.connect(
+            host = os.getenv("DB_HOST"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            db = os.getenv("DB_NAME")
+        )
+
+        self.cursor = self.db_connect.cursor()
+    def request(self):
+        self.cursor.execute("SHOW TABLES")
+        print(self.cursor.fetchall())
+
+    def close_db(self):
+        self.cursor.close()
+        self.db_connect.close()
+
+
+db = DatabaseManager()
+db.request()
