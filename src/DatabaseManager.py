eb728ae06be60066c9ffd52110a778849c38f049
hugo-belaloui <hugo.belaloui@laplateforme.io>
2026-03-17 12:23:25 +0100

    feat(db): refactor DatabaseManager and add user query support
      Refactor DatabaseManager to initialize database connections in the constructor and improve request handling:
       - Initialize connection in __init__ instead of a separate method.
       - Update run_request to automatically handle commits for write operations and fetching for read operations.
       - Add basic error handling for database queries.
       - Implement desc_tables and get_user methods.

--- src/DatabaseManager.py
+++ src/DatabaseManager.py
@@ -4,7 +4,7 @@ import dotenv
 
 class DatabaseManager: 
 
-    def connect_db(self):
+    def __init__(self):
         dotenv.load_dotenv()
         self.db_connect = mysql.connector.connect(
             host = os.getenv("DB_HOST"),
@@ -13,22 +13,31 @@ class DatabaseManager:
             db = os.getenv("DB_NAME")
         )
 
-    def run_request(self, request, data): #takes as param the query and the data
-        self.connect_db()
-        self.cursor = self.db_connect.cursor()
-        self.cursor.execute(request, data)
-        print(self.cursor.fetchall())
-        self.close_db()
-        #TODO add a return fetchall()
 
-    def show_tables(self):
-        data = "SHOW TABLES"
+    def run_request(self, request, data): #takes as param the query and the data
+        self.cursor = self.db_connect.cursor()
+        try: 
+            self.cursor.execute(request, data) #execute
+            if request.lower().startswith(("insert", "update", "delete")): #handle commits if CUD query
+                self.db_connect.commit()
+            if request.lower().startswith(("desc", "show", "select")): #handle fetch if READ query
+                print(self.cursor.fetchall())
+        except mysql.connector.Error as error :
+            print("DB Error: ", error)
+
+    def desc_tables(self):
+        data = "DESC ACCOUNT"
         return data
+    
+    def get_user(self):
+        rqst = "SELECT id_user FROM account WHERE id = %s" #%s is a placeholder
+        return self.run_request(rqst, (0,))
+
     def close_db(self):
         self.cursor.close()
         self.db_connect.close()
 
 
 db = DatabaseManager()
-db.run_request(db.show_tables(), None)
-
+db.run_request(db.desc_tables(), None)
+db.get_user()
